ce7f2c2462a3ab759c028901cce10003393b3a2e
sajjad-amin <sayem.1998.bd@gmail.com>
2023-02-14 21:23:01 +0600

    ssl added

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+require('dotenv').config();
+const fs = require('fs');
 const { PeerServer } = require('peer');
-const peerServer = PeerServer({ port: 1001, path: '/' });
+const sslEnabled = process.env.SSL_ENABLED === 'true';
+let peerConfig = {
+    port: 9000
+}
+if (sslEnabled) {
+    peerConfig.ssl = {
+        key: fs.readFileSync(process.env.SSL_KEY),
+        cert: fs.readFileSync(process.env.SSL_CERT)
+    }
+}
+const peerServer = PeerServer(peerConfig);
